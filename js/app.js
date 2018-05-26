6351817759f5b2b8e99feb837234acd6846b33dd
Dimitra Karamperi <dkaramperi@yahoo.gr>
2018-05-27 00:35:07 +0300

    Added Instructions Modal

--- js/app.js
+++ js/app.js
@@ -25,6 +25,36 @@ function init() {
 }
 
 
+// Instructions Modal
+// https://www.w3schools.com/howto/howto_css_modals.asp
+
+// Get the modal
+let modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+let btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
 // Click Event
 
 // First Click Indicator
